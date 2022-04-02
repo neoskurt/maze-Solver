bd1de0e37cc2de49e5216c193ab8b5d99b5f337d
neoskurt <49443524+neoskurt@users.noreply.github.com>
2022-04-02 13:00:14 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,49 +4,44 @@ import Map from './map';
 
 const timings = {
     oval: <number[]>[],
-    square: <number[]>[],
+    rect: <number[]>[],
 };
 
-for (let i = 1; i <= 5; i++) {
-    const start = Date.now(),
-        map = Map.from('rect', i);
 
-    explore(map);
-    const end = Date.now();
+(async function () {
+    await testLabyrinths("rect", 1, 5);
+    await testLabyrinths("oval", 1, 1);
 
-    timings.square[i] = end - start;
+    log("\n\nTimings:\n");
+    printTimings("rect");
+    log();
+    printTimings("oval");
+})();
 
-    log(
-        `\n\nRectangle #${i}:\n\n` +
-        map.toString()
-    );
+async function testLabyrinths(shape: "oval" | "rect", start: number, end: number) {
+    for (let i = start; i <= end; i++) {
+        const start = Date.now(),
+            map = Map.from(shape, i);
+
+        await explore(map);
+        const end = Date.now();
+
+        timings[shape][i] = end - start;
+
+        log(
+            `\n${shape[0].toUpperCase() + shape.slice(1).toLowerCase()} #${i}:\n\n` +
+            map.toString()
+        );
+    }
 }
 
-for (let i = 1; i <= 1; i++) {
-    const start = Date.now(),
-        map = Map.from('oval', i);
 
-    explore(map);
-    const end = Date.now();
-
-    timings.oval[i] = end - start;
-
-    explore(map);
-    log(
-        `\nOval #${i}:\n\n` +
-        map.toString()
-    );
+function printTimings(shape: "oval" | "rect") {
+    for (const i in timings[shape]) {
+        const timing = timings[shape][i];
+        log(
+            `${shape[0].toUpperCase() + shape.slice(1).toLowerCase()} #${i}:`,
+            timing < 1000 ? timing + 'ms' : timing / 1000 + 's'
+        );
+    }
 }
-
-log("\n\nTimings:\n");
-
-for (const i in timings.square) {
-    const timing = timings.square[i];
-    log(`Square #${i}:`, timing < 1000 ? timing + 'ms' : timing / 1000 + 's');
-}
-
-for (const i in timings.oval) {
-    const timing = timings.oval[i];
-    log(`Oval #${i}:`, timing < 1000 ? timing + 'ms' : timing / 1000 + 's');
-}
-
